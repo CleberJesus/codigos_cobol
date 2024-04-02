f7903bb40ead5d9d3069ac187308a60a027385c6
Cleber Jesus <cbcj2022@gmail.com>
2024-04-02 13:23:34 -0300

    Comentários

--- teste.cob
+++ teste.cob
@@ -3,11 +3,15 @@
       * DATA:  02/04/2024
       ***********************************************
         IDENTIFICATION DIVISION.
-        PROGRAM-ID. teste.
+        PROGRAM-ID. TESTE.
         DATA DIVISION.
         WORKING-STORAGE SECTION.
+      * DECLARAÇÃO DE VARIÁVEIS
         77 WS-NOME      PIC X(10).
         PROCEDURE DIVISION.
+      * COMANDO DISPLAY EXIBE NA TELA
+      * COMANDO ACCEPT ENTRADA DE DADOS
+      * STOP RUN ENCERRAR PROCESSO
            DISPLAY "DIGITE O NOME: "
            ACCEPT WS-NOME
            DISPLAY "O NOME DIGITAO É: " WS-NOME
